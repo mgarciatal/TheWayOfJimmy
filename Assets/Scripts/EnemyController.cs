7d21fa89f71a1383ba625107da03df7d1d6436cb
mgarciatal <miquel_barsa@hotmail.com>
2019-10-08 10:27:03 +0200

    adding my files to my repository

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    public float rotationSpeed = 1f;
+
+    Vector3 targetPosition;
+    Vector3 towardsTarget;
+
+    float wanderRadius = 5f;
+
+
+    void RecalculateTargetPosition ()
+    {
+        targetPosition = transform.position + Random.insideUnitSphere * wanderRadius;
+        targetPosition.y = 0;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        RecalculateTargetPosition();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        towardsTarget = targetPosition - transform.position;
+        if (towardsTarget.magnitude < 0.25f)
+        RecalculateTargetPosition();
+
+        Quaternion towardsTargetRotation = Quaternion.LookRotation(towardsTarget, Vector3.up);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, towardsTargetRotation, rotationSpeed * Time.deltaTime);
+            
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+
+        Debug.DrawLine(transform.position, targetPosition, Color.green);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalmove;
+    public float verticalmove;
+    public CharacterController Player;
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalmove = Input.GetAxis("Horizontal");
+        verticalmove = Input.GetAxis("Vertical");
+
+    }
+            private void FixedUpdate() {
+        Player.Move(new Vector3(horizontalmove, 0, verticalmove)*playerSpeed*Time.deltaTime);
+
+
+    }
+
+}
+
