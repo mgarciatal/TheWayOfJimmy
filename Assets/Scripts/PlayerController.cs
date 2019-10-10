ec15da279be522acd41d1d8d26779e642582fcf1
mgarciatal <miquel_barsa@hotmail.com>
2019-10-10 17:11:45 +0200

    Update 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalmove;
-    public float verticalmove;
-    public CharacterController Player;
-    public float playerSpeed;
+    public GameObject Cam;
+    public GameObject CamPost;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GetComponent<CharacterController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalmove = Input.GetAxis("Horizontal");
-        verticalmove = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, 0.1f));
+        }
+        else {
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, 0, -0.1f));
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            CamPost.transform.SetParent(null);
+            transform.Rotate(new Vector3(0, 5.0f, 0));
+            CamPost.transform.SetParent(transform);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                CamPost.transform.SetParent(null);
+                transform.Rotate(new Vector3(0, -5.0f, 0));
+                CamPost.transform.SetParent(transform);
+            }
+        }
 
+        Cam.transform.position = Vector3.Lerp(Cam.transform.position, CamPost.transform.position, 0.1f);
     }
-            private void FixedUpdate() {
-        Player.Move(new Vector3(horizontalmove, 0, verticalmove)*playerSpeed*Time.deltaTime);
-
-
-    }
+            
 
 }
 
