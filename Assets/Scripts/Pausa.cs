7ab9b962a64a4dea60711337a657b60cb1b30a66
mgarciatal <miquel_barsa@hotmail.com>
2019-10-11 18:03:00 +0200

    Update 2

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausa : MonoBehaviour
+{
+    private bool active;
+    Canvas canvas;
+    private bool musica;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            active = !active;
+            canvas.enabled = active;
+            Time.timeScale = (active) ? 0 : 1f;
+            AudioListener.volume = 0;
+        }
+        
+        
+            
+     } 
+    
+}
