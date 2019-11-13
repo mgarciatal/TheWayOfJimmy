65e576596d9a386d80314fe1d62f81226b9ec923
mgarciatal <miquel_barsa@hotmail.com>
2019-11-13 18:38:07 +0100

    Update 14

---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    private bool active;
+    Canvas Canvas_Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas_Text = GetComponent<Canvas>();
+        Canvas_Text.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            active = !active;
+            Canvas_Text.enabled = active;
+            
+
+        }
+    }
+}
