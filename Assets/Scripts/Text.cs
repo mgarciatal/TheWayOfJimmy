515034810d5b4f94c8ea38c43841e9d66d86270b
mgarciatal <miquel_barsa@hotmail.com>
2019-11-26 17:43:17 +0100

    Update 19

--- Assets/Scripts/Text.cs
+++ Assets/Scripts/Text.cs
@@ -6,6 +6,9 @@ public class Text : MonoBehaviour
 {
     private bool active;
     Canvas Canvas_Text;
+    public float temps_start;
+    public float temps_end;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +20,11 @@ public class Text : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("t"))
+        temps_start += Time.deltaTime;
+
+        if (temps_start >= temps_end )
         {
-            active = !active;
-            Canvas_Text.enabled = active;
+            Destroy(gameObject); 
             
 
         }
