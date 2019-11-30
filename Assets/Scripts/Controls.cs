c98c70b34c06bdd3a3ad5854751d65fd1977fab0
mgarciatal <miquel_barsa@hotmail.com>
2019-11-30 11:23:59 +0100

    Update 23

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    private bool active;
+    Canvas Canvas_Controls;
+    public float temps_start;
+    public float temps_end;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas_Controls = GetComponent<Canvas>();
+        Canvas_Controls.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temps_start += Time.deltaTime;
+
+        if (temps_start >= temps_end)
+        {
+            Destroy(gameObject);
+
+
+        }
+    }
+}
